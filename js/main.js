9efcf6c003fd740dbfcedb2ca7440fbe920ed21c
Mansour Almohsen <mansouralmohsen@Mansours-MacBook-Pro.local>
2019-05-31 13:42:29 +0300

    created logic for the game

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
 var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
 
-if (cardsInPlay.length === 2){
-	if (cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
-	}else {
-		alert("Sorry, try again.");
+function flipCard(cardId) {
+
+	var cardOne = cards[0];
+	cardsInPlay.push(cardOne);
+	console.log("User flipped " + cardOne);
+
+	var cardTwo = cards[2];
+	cardsInPlay.push(cardTwo);
+	console.log("User flipped " + cardTwo);
+
+	if (cardsInPlay.length === 2){
+		if (cardsInPlay[0] === cardsInPlay[1]){
+			alert("You found a match!");
+		}else {
+			alert("Sorry, try again.");
+		}
 	}
+	console.log("User flopped " + cards[cardId]);
 }
+flipCard(2);
 
-console.log("User flipped " + cardsInPlay);
 
