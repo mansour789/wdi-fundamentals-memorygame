a8c62c743112718f6b5b124f58b57be97dfb7c49
Mansour Almohsen <mansouralmohsen@Mansours-MacBook-Pro.local>
2019-05-31 14:19:04 +0300

    created logic to the game

--- js/main.js
+++ js/main.js
@@ -1,26 +1,68 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "queen",
+		usit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		usit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		usit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		usit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 
 var cardsInPlay = [];
 
-function flipCard(cardId) {
+function checkForMath(){
 
-	var cardOne = cards[0];
-	cardsInPlay.push(cardOne);
-	console.log("User flipped " + cardOne);
-
-	var cardTwo = cards[2];
-	cardsInPlay.push(cardTwo);
-	console.log("User flipped " + cardTwo);
-
-	if (cardsInPlay.length === 2){
-		if (cardsInPlay[0] === cardsInPlay[1]){
-			alert("You found a match!");
-		}else {
-			alert("Sorry, try again.");
-		}
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	 	 //console.log
+	 	 alert("You found a match!");
+	} else {
+	 	//console.log
+	 	alert("Sorry, try again.");
 	}
-	console.log("User flopped " + cards[cardId]);
 }
-flipCard(2);
+
+function flipCard() {
+
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
+
+
+	if (cardsInPlay.length > 1){
+			if (cardsInPlay[0] === cardsInPlay[1]){
+				checkForMath();
+				//alert('You found a match!');
+			}else {
+				//alert(' Sorry, try again.');
+			checkForMath();
+			}
+	}
+
+	cardsInPlay.push(cards[cardId].rank);
+	
+}
+function createBoard() {
+	for (var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
 
 
